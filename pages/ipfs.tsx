2b9f2cd7a25aa60d49ac8a9c3b195c31510ca994
baixingxing <baixingxing@onething.net>
2025-04-03 10:31:51 +0800

    IPFS: 个人服务器搭建ipfs

--- pages/ipfs.tsx
+++ pages/ipfs.tsx
@@ -4,17 +4,19 @@ import { Input, Button, Card, Image, message, Select, Upload } from "antd";
 import { UploadOutlined } from "@ant-design/icons";
 import type { UploadProps } from "antd";
 
-const ipfsGateway = "http://localhost:8080/ipfs/";
 
 const ipfsGateways = {
-  local: "http://localhost:8080/ipfs/",
+  // local: "http://localhost:8080/ipfs/",
   pinata: "https://tan-legal-lungfish-82.mypinata.cloud/ipfs/",
+  dev: "http://117.72.119.181:8080/ipfs/",
 };
 const uploadGateways = {
   local: {
     name: "file",
     action: "http://localhost:5001/api/v0/add",
-    headers: {},
+    headers: {
+      
+    },
   },
   pinata: {
     action: "https://api.pinata.cloud/pinning/pinFileToIPFS",
@@ -22,15 +24,21 @@ const uploadGateways = {
       Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI4ZTVkZGJiYi1mMTNmLTQxOWQtODNiYi1kM2Q5YzZkZjk5ZWYiLCJlbWFpbCI6ImJ4eHN5ZWFAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjEsImlkIjoiRlJBMSJ9LHsiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjEsImlkIjoiTllDMSJ9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6Ijc2YTZjNTZlMjY0OTQwYzdjYzNjIiwic2NvcGVkS2V5U2VjcmV0IjoiNWM4OWQ3YjY5MmY3YWY3Y2I5YmIzNTlkMWZhYjBiZTM5NWNhYjZiODc2ZjA3NmYzOWY4ZTUwNWU3N2UzMDQ4YSIsImV4cCI6MTc3NDY4Njc3OH0.4pCqifuqZiuQC0hpNUB1llAtVPb_i_wkfQiKSSVH24A`,
     },
   },
+  dev: {
+    name: "file",
+    action:
+      "http://117.72.119.181:5001/api/v0/add",
+    withCredentials: false,
+  },
 };
 function Ipfs() {
   const [cid, setCid] = useState(
-    "QmYrD8FyJUhbApkxovgnhQUrPZHY7JqQd3YUN6o3sPJtwr",
+    "QmTQak529xYZjwnZowEoew2Sr125waFAUonFwVq3ej5A5H",
   );
   const [imageUrl, setImageUrl] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const [gateway, setGateway] = useState("local");
+  const [gateway, setGateway] = useState("dev");
 
   const handleViewImage = () => {
     if (!cid) {
@@ -53,7 +61,23 @@ function Ipfs() {
     async onChange(info) {
       if (info.file.status === "done") {
         message.success(`${info.file.name} 上传成功`);
-        await setCid(info.file.response.Hash || info.file.response.IpfsHash);
+           const hash = info.file.response.Hash || info.file.response.IpfsHash;
+           await setCid(hash);
+        await setCid(hash);
+
+        try {
+          const response = await fetch(`http://117.72.119.181:5001/api/v0/files/cp?arg=/ipfs/${hash}&arg=/${info.file.name}`,
+            {
+              method: "POST",
+            }
+          );
+          if (response.ok) {
+            message.success("文件已保存到 IPFS MFS");
+          }
+        } catch (error) {
+          console.error("保存到 MFS 失败:", error);
+        }
+        
         await handleViewImage()
       } else if (info.file.status === "error") {
         message.error(`${info.file.name} 上传失败`);
@@ -63,7 +87,23 @@ function Ipfs() {
   return (
     <div>
       <h1>IPFS图片查看器</h1>
-      <Card title="查看 IPFS 图片" style={{ marginBottom: "20px" }}>
+      <Card
+        title={
+          <div style={{display:'flex'}}>
+            查看 IPFS 图片
+            <span style={{flex: 1, textAlign: 'right'}}>
+              <a
+                style={{ color: "blue" }}
+                href="http://117.72.119.181:5001/ipfs/bafybeicyp7ssbnj3hdzehcibmapmpuc3atrsc4ch3q6acldfh4ojjdbcxe/#/files"
+                target="_blank"
+              >
+                http://117.72.119.181:5001/ipfs/bafybeicyp7ssbnj3hdzehcibmapmpuc3atrsc4ch3q6acldfh4ojjdbcxe/#/files
+              </a>
+            </span>
+          </div>
+        }
+        style={{ marginBottom: "20px" }}
+      >
         <Input
           placeholder="输入 IPFS CID (例如: QmW2WQi7j6c7UgJTarActp7tDNikE4B2qXtFCfLPdsgaTQ)"
           value={cid}
@@ -84,17 +124,28 @@ function Ipfs() {
             );
           })}
         </Select>
-        <Button type="primary" onClick={handleViewImage} loading={loading} style={{marginRight: 14}}>
+        <Button
+          type="primary"
+          onClick={handleViewImage}
+          loading={loading}
+          style={{ marginRight: 14 }}
+        >
           查看图片
         </Button>
         <Upload {...uploadProps}>
           <Button icon={<UploadOutlined />}>上传图片-{gateway}</Button>
         </Upload>
-        <div>
-          <p>local：QmYrD8FyJUhbApkxovgnhQUrPZHY7JqQd3YUN6o3sPJtwr</p>
+        <h3 style={{ lineHeight: "300%" }}>
+          Gateway：{" "}
+          {typeof uploadProps.action === "string" ? uploadProps.action : "未知"}
+        </h3>
+        <h3>图片URL示例：</h3>
+        <div style={{ paddingLeft: "24px" }}>
+          {/* <p>local：QmYrD8FyJUhbApkxovgnhQUrPZHY7JqQd3YUN6o3sPJtwr</p> */}
           <p>
             pinata：bafybeigszmwipyfxvaaftinawfiog7clp3b47vsjr3dyuh3hgatjlfi4om
           </p>
+          <p>dev：QmTQak529xYZjwnZowEoew2Sr125waFAUonFwVq3ej5A5H</p>
         </div>
       </Card>
       {imageUrl && (
